e5684a4425d9d9dc7953f58234e6822c513e896d
Shaoting Fong <fengshaoting@fengshaotingde-MacBook-Pro.local>
2023-07-28 14:00:00 +0800

    Delete Unused files.

--- server/utils/messages.ts
+++
@@ -1,9 +0,0 @@
-import dayjs from "dayjs";
-
-const formatMessage = (username: string, text: string): object => ({
-  username,
-  text,
-  time: dayjs().format("HH:mm a"),
-});
-
-export default formatMessage;
